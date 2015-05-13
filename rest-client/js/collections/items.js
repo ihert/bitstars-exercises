26417a2f53637b2718840fc9f18b79eb73332714
ihert <igor.hertfelder@rwth-aachen.de>
2015-05-13 11:56:02 +0200

    add url for collection

--- rest-client/js/collections/items.js
+++ rest-client/js/collections/items.js
@@ -5,6 +5,10 @@ define([
 ], function(_, Backbone, Item){
 
     var ItemsCollection = Backbone.Collection.extend({
+
+        url: 'http://2-dot-crowdev-template.appspot.com/v1/tests/',
+
+        model: Item
     });
 
     return new ItemsCollection;
