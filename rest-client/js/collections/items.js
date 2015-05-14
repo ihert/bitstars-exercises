87de52a307093dcc090cd10a2e4e09ea75d23ac2
ihert <igor.hertfelder@rwth-aachen.de>
2015-05-14 15:01:48 +0200

    implement comparator: asc/desc way of sorting

--- rest-client/js/collections/items.js
+++ rest-client/js/collections/items.js
@@ -8,7 +8,23 @@ define([
 
         url: 'http://2-dot-crowdev-template.appspot.com/v1/tests/',
 
-        model: Item
+        model: Item,
+
+        sortAsc: true,
+        
+        comparator: function (item) {
+            if(this.sortAsc) {
+                return  item.get('name');
+            } else {
+                var str = item.get('name');
+                str = str.toLowerCase();
+                str = str.split('');
+                str = _.map(str, function(letter) {
+                    return String.fromCharCode(-(letter.charCodeAt(0)));
+                });
+                return str;
+            }
+        }
     });
 
     return new ItemsCollection;
